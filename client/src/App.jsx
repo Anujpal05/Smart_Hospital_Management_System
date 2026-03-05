23c04caccbb814eea3d62c051942b3a738d0df6d
Anuj Pal <ap091453@gmail.com>
2026-03-05 16:03:57 +0530

    Creating Services , Contact, Doctors, SingleService page and also fix some bugs in about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import Home from './Pages/Home'
-import Navbar from './Components/Home/Navbar';
-import About from './Pages/About';
-import Footer from './Components/Home/Footer';
-import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import React from "react";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Home/Navbar";
+import About from "./Pages/About";
+import Footer from "./Components/Home/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Services from "./Pages/Services";
+import SingleService from "./Pages/SingleService";
+import Doctors from "./Pages/Doctors";
+import Contact from "./Pages/Contact";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-
-        </Routes>
-        <Footer/>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/free-checkup" element={<SingleService />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
